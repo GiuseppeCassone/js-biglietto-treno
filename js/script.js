ca4648b0763208040e201496844d3f1d5800c4e3
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-01-26 18:16:58 +0100

    inserito anche le condizionali per il bonus

--- js/script.js
+++ js/script.js
@@ -16,15 +16,32 @@ const age = prompt("Quanti anni hai?")
 
 document.getElementById(`age`).innerHTML = age;
 
-if (age < 18) {
+if (!isNaN(age) && Number.isInteger(Number(age)) && (age>0 && age < 110) && !isNaN(distance) && Number.isInteger(Number(distance)) && (distance > 0)){
+    
+    if (age < 18) {
+    
+        document.getElementById(`yourDiscount`).innerHTML = `Hai i requisiti per uno sconto del 20%`
+    
+        basePrice = (basePrice * 0.20)
+    }
 
-    basePrice = (basePrice * 0.20)
+    if (age > 17 && age < 65) {
+
+        document.getElementById(`yourDiscount`).innerHTML = `Non hai i requisiti per uno sconto`
+    }
+    
+    if (age > 64) {
+    
+        document.getElementById(`yourDiscount`).innerHTML = `Hai i requisiti per uno sconto del 40%`
+    
+        basePrice = (basePrice * 0.40)
+    }
+    
+    document.getElementById(`finalPrice`).innerHTML = `Il prezzo del tuo biglietto è di ${basePrice.toFixed(2)}`;
+
+} else {
+
+    document.getElementById(`finalPrice`).innerHTML = `Non hai inserito i dati in maniera corretta!`;
 }
 
-if (age > 64) {
-
-    basePrice = (basePrice * 0.40)
-}
-
-document.getElementById(`finalPrice`).innerHTML = `Il prezzo del tuo biglietto è di ${basePrice.toFixed(2)}`;
 
